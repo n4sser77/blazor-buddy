b017d65f0d4f4a7ebfab356ba327590a9fc51ebf
Noel Burman <noelburman@hotmail.com>
2025-11-25 14:45:53 +0100

    add CustomWebApplication + first test for Notes

---
+++ BlazorBuddy.Test/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using BlazorBuddy.WebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BlazorBuddy.Test.Integration
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                
+                services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+                services.RemoveAll(typeof(ApplicationDbContext));
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDatabase_" + Guid.NewGuid().ToString());
+                });
+
+                var serviceprovider = services.BuildServiceProvider();
+
+                using (var scope = serviceprovider.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    db.Database.EnsureCreated();
+
+                    
+                }
+            });
+        }
+    }
+}
